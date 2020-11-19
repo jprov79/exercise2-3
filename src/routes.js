f0a63bba1341424520559f1a4fcfef925dcae987
jprov79 <71113610+jprov79@users.noreply.github.com>
2020-11-18 18:21:10 -0700

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,60 @@
+"use strict";
+
+module.exports.register = (app, database) => {
+
+    app.get('/', async (req, res) => {
+        res.status(200).send("You did it! I am now running:) ").end();
+    });
+
+
+    app.get('/api/emp', async (req, res) => {
+        console.log("=================");
+        let query;
+        if (req.query.name) {
+            let _name = req.query.name;
+            query = database.query(
+                'select * from rest_emp where name = ?',
+                [_name]
+            );
+        } else {
+            query = database.query(
+                'SELECT * FROM rest_emp'
+            );
+        }
+        console.log(query);
+        const emps = await query;
+
+        res.status(200).send(JSON.stringify(emps)).end();
+    });
+
+
+
+    app.get('/api/emp/:id', async (req, res) => {
+        let _id = req.params.id;
+        const query = database.query(
+            'select * from rest_emp where id = ?',
+            [_id]
+        );
+        const emps = await query;
+        res.status(200).send(JSON.stringify(emps)).end();
+    });
+
+
+
+    app.post('/api/emp', async (req, res) => {
+        let _name = req.body.name;
+        let _phone = req.body.phone;
+        let _email = req.body.email;
+        let _address = req.body.address;
+
+        const query = database.query(
+            'insert into rest_emp(name, phone, email, address) values (?, ?, ?, ?)',
+            [_name, _phone, _email, _address]
+        );
+        const emps = await query;
+        res.status(200).send('Employee added successfully!').end();
+    });
+
+
+
+};
